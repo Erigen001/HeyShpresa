6ba242b555e5e81f9dc1c2ad61eb05cfb39cf6ba
andi <andi@git.email>
2022-11-10 20:49:13 +0100

    10-11-2022 coding

--- shpresa.sh
+++ shpresa.sh
@@ -3,6 +3,11 @@
 USERNAME_FILENAME="username.file"
 JOKES_SCRIPT_PATH="resources/jokes/jokes.sh"
 WEATHER_SCRIPT_PATH="resources/weather/weather.sh"
+ACTIVITY_SCRIPT_PATH="resources/activity/activity.sh"
+LYRICS_FOLDER_PATH="resources/lyrics"
+IS_NUMBER_EXPR='^[0-9]+$'
+MAIN_MENU_EXIT_VALUE=1
+
 
 USERNAME=$(<$USERNAME_FILENAME)
 
@@ -17,7 +22,7 @@ echo "Hello there $USERNAME"
 
 #Functions
 function displayMenu () {
-	declare -a menuItems=( 'Show me the weather for next week!' 'Tell me a joke!' 'Show me the date!' 'Show me song lyrics!' 'Exit' )
+	declare -a menuItems=( 'Show me the weather for next week!' 'Tell me a joke!' 'Show me the date!' 'Show me song lyrics!' 'Whan activity could I do today?' )
 
 	COUNTER=1
 	for ((i=0; i < ${#menuItems[@]}; i++))
@@ -25,6 +30,10 @@ function displayMenu () {
 		echo "($COUNTER) ${menuItems[$i]}"
 		((COUNTER++))
 	done
+
+	((MAIN_MENU_EXIT_VALUE=COUNTER))
+
+	echo "($COUNTER) Exit"
 }
 
 function checkAndExecScript () {
@@ -39,6 +48,52 @@ function checkAndExecScript () {
                 source $FILEPATH
         fi
 }
+
+function displaySongList () {
+	songFiles=$1
+	
+	echo ""
+        echo "--------SONG LIST---------"
+	COUNTER=1
+        for ((i=0; i<${#songFiles[@]}; i++))
+        do
+                echo "($COUNTER) ${songFiles[$i]}"
+        	((COUNTER++))
+        done
+        echo "($COUNTER) Go back"
+	echo "--------------------------"
+}
+
+function displaySongListMenu () {
+	songFiles=($(ls $LYRICS_FOLDER_PATH))
+
+	while true
+	do	
+		displaySongList $songFiles
+
+		read -p "Choose a song to display its lyrics or choose 'Go back' to return to the main menu: " SONGOPTION
+
+		case "$SONGOPTION" in
+		"$COUNTER")
+			echo "Exiting song menu"
+			sleep 0.8
+			break;;
+		*)
+			sleep 0.8
+			if [[ $SONGOPTION =~ $IS_NUMBER_EXPR && $SONGOPTION -ge 0 && $SONGOPTION -lt ${#songFiles[@]} ]]
+			then
+				((SONGOPTION--))
+				echo ""
+				cat $LYRICS_FOLDER_PATH/${songFiles[$SONGOPTION]}
+				echo ""
+				read -n 1 -r -s -p "Press any key to display song selection menu..." key
+			else				
+	                	echo "Please input a correct value!"
+			fi
+		esac
+	done
+}
+
 #
 
 #Logic
@@ -67,7 +122,15 @@ do
 		sleep 0.8
 		echo "Today's date is $(date)"
 		;;
-        "5")
+	"4")	
+		sleep 0.8
+		displaySongListMenu
+		;;
+	"5")
+		sleep 0.8
+		checkAndExecScript $ACTIVITY_SCRIPT_PATH
+		;;
+	"$MAIN_MENU_EXIT_VALUE")
 		sleep 0.8
                 echo "Exiting, goodbye $USERNAME!"
                 exit 0;;
